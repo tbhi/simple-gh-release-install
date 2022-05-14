637dd84f4078910385bcdfcc6eb5265b4c0e4d60
Tristan Hill <t+git@eaux.uk>
2022-05-14 21:01:53 +0100

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Simple Github Release Installer
+
+Tested with
+- restic/restic
+- restic/rest-server
+- mozilla/geckodriver
+
+## Similar
+- https://github.com/jooola/gh-release-install
---
+++ simple-gh-release-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -o pipefail
+
+REPO=$1
+ARCH="linux_amd64|linux64"
+INSTALLED=$HOME/.simple-gh-release-installll
+
+latest_url="$(wget -qO - https://api.github.com/repos/$REPO/releases/latest | egrep "browser_download_url.*($ARCH)" | sed -n '1s/.*"\([^"]*\)"$/\1/p')"
+
+xinstall() {
+  case "$1" in
+    *.tar.gz)
+      tar xz --no-same-owner --strip-components 1 --wildcards \*$(basename $REPO)
+      ;;
+    *.bz2)
+      bunzip2 > "$(basename $REPO)"
+      chmod +x "$(basename $REPO)"
+      ;;
+  esac    
+}
+
+[ -z "$latest_url" ] && exit 1
+
+if ! grep -q "$latest_url" "$INSTALLED"; then
+  wget -qO - "$latest_url" | xinstall "$latest_url"
+  echo "$latest_url" >> "$INSTALLED"
+fi
