782c4f5a5f399ab9e9131c1fe16faeafb1014585
Tristan Hill <t+git@eaux.uk>
2022-08-25 20:07:25 +0100

    correct restic install, add help

--- simple-gh-release-install.sh
+++ simple-gh-release-install.sh
@@ -10,13 +10,15 @@ die() {
 }
 
 xinstall() {
-  case "$1" in
+  local latest_url="$1"
+  local repo=$2
+  case "$latest_url" in
     *.tar.gz)
       tar xz --no-same-owner --strip-components 1 --wildcards \*$(basename $REPO)
       ;;
     *.bz2)
-      bunzip2 > "$(basename $REPO)"
-      chmod +x "$(basename $REPO)"
+      bunzip2 > "$(basename $repo)"
+      chmod +x "$(basename $repo)"
       ;;
   esac    
 }
@@ -26,17 +28,23 @@ ghinstall() {
   local latest_url="$(wget -qO - https://api.github.com/repos/$repo/releases/latest | egrep "browser_download_url.*($ARCH)" | sed -n '1s/.*"\([^"]*\)"$/\1/p')"
   [ -z "$latest_url" ] && die "empty latest url - hit github quota?"
   if ! [ -f "$INSTALLED" ] || ! grep -q "$latest_url" "$INSTALLED"; then
-    wget -qO - "$latest_url" | xinstall "$latest_url"
+    wget -qO - "$latest_url" | xinstall "$latest_url" "$repo"
     echo "installed $latest_url"
     echo "$latest_url" >> "$INSTALLED"
   fi
 }
 
-if [ "$1" = "all" ]; then
-  [ -f "$INSTALLED" ] || die "no previously installed"
-  for repo in $(sed 's|.*github.com\/\([^/]*\/[^/]*\)\/.*|\1|' "$INSTALLED" | sort -u); do
-    ghinstall $repo
-  done
-else
-  ghinstall "$1"
-fi
+case "$1" in
+  all)
+    [ -f "$INSTALLED" ] || die "no previously installed"
+    for repo in $(sed 's|.*github.com\/\([^/]*\/[^/]*\)\/.*|\1|' "$INSTALLED" | sort -u); do
+      ghinstall $repo
+    done
+    ;;
+  "")
+    die "Usage: $0 all|github_project/name"
+    ;;
+  *)
+    ghinstall "$1"
+    ;;
+esac
