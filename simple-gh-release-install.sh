eb5230642c2402f0aba960be8a1e239fdd8bae19
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-12-25 04:09:40 +0000

    codeberg support

--- simple-gh-release-install.sh
+++ simple-gh-release-install.sh
@@ -3,7 +3,10 @@ set -e -o pipefail
 
 case "$(uname -m)" in
   x86_64)
-    REGEX="linux.*amd64|linux64|linux_x86_64|ubuntu-latest-minimal|_x64_|centos-9-stream\.rpm"
+    REGEX="linux.*amd64|linux64|linux_x86_64|ubuntu-latest-minimal|_x64_|amd64\.deb"
+    ;;
+  armv7l)
+    REGEX="linux_arm|linux_armv7"
     ;;
 esac
 INSTALLED=$HOME/.simple-gh-release-install
@@ -48,12 +51,28 @@ xinstall() {
       find "$tmp" -type f -perm /u+x -exec mv {} . \;
       rm -rf "$tmp"
       ;;
+     *)
+      local name="$(basename "$repo")"
+      cat > "$name"
+      chmod +x "$name"
+      ;;
   esac
 }
 
-ghinstall() {
+repo_install() {
   local repo=$1
-  local latest_url="$(wget -qO - https://api.github.com/repos/$repo/releases/latest | grep -Ei "browser_download_url.*($REGEX)" | sed -n '1s/.*"\([^"]*\)"$/\1/p')"
+  local api_url
+  case "$repo" in
+    codeberg/*)
+      repo="${repo#*/}"
+      api_url="https://codeberg.org/api/v1/repos/$repo/releases/latest"
+      ;;
+    *)
+      [[ "$repo" ==https://github.com/tbhi/simple-gh-release-install/tree/master */*/* ]] && repo="${repo#*/}"
+      api_url="https://api.github.com/repos/$repo/releases/latest"
+      ;;
+  esac
+  local latest_url=$(wget -qO - "$api_url" | jq -r '.assets[].browser_download_url' | grep -Em1 "$REGEX")
   [ -z "$latest_url" ] && die "empty latest url - hit github quota?"
   if ! [ -f "$INSTALLED" ] || ! grep -q "$latest_url" "$INSTALLED"; then
     wget -qO - "$latest_url" | xinstall "$latest_url" "$repo"
@@ -65,15 +84,15 @@ ghinstall() {
 case "$1" in
   all)
     [ -f "$INSTALLED" ] || die "no previously installed"
-    for repo in $(sed 's|.*github.com\/\([^/]*\/[^/]*\)\/.*|\1|' "$INSTALLED" | sort -u); do
+    for repo in $(sed -E 's#^https?://([^.]+)\.[^/]+/([^/]+/[^/]+)/.*$#\1/\2#' "$INSTALLED" | sort -u); do
       echo checking $repo
-      ghinstall $repo
+      repo_install $repo
     done
     ;;
   "")
     die "Usage: $0 all|github_project/name"
     ;;
   *)
-    ghinstall "$1"
-    ;;
+    repo_install "$1"
+    ;;  
 esac
